cd939e26a22f23c508b8125edc58c84e4ed68b60
Happy-Darpy <injinsan@gmail.com>
2024-04-29 18:39:07 -0400

    1

--- test.php
+++ test.php
@@ -2,6 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
 session_start();
 
 $username = trim($_POST['UserName']);
@@ -11,7 +12,7 @@ $username = trim($_POST['UserName']);
   $email = trim($_POST['email']);
   $password = hash('sha256', $pass);
 
-  error_log("username is :" . $username);
+  echo "username is :" . $username;
 
   session_destroy();
 

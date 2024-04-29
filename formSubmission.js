4a9caec6353465bbadfac65c504f47149b3f498a
Happy-Darpy <injinsan@gmail.com>
2024-04-29 17:45:22 -0400

    lll

--- formSubmission.js
+++ formSubmission.js
@@ -1,11 +1,11 @@
-function preventNavigation() {
+const email = localStorage.getItem("email");
+const textField = document.getElementById("email");
+
+
+function loadbody() {
     history.pushState(null, null, document.URL);
     window.addEventListener('popstate', function () {
       history.pushState(null, null, document.URL);
     });
+    textField.innerHTML = email;
   };
-
-  const email = localStorage.getItem("email");
-  console.log("email is" + email);
-  const textField = document.getElementById("email");
-  textField.textContent = email;
